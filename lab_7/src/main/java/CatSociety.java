947ec88714bc3fee6f96474ba9a4655944d925d3
Ahmad Fauzan Amirul Isnain <ahmad.fauzan4580@gmail.com>
2018-11-10 13:45:03 +0700

    Update CatSociety

--- lab_7/src/main/java/CatSociety.java
+++ lab_7/src/main/java/CatSociety.java
@@ -5,6 +5,34 @@ import character.KucingPrajurit;
 
 public class CatSociety {
     public static void main(String[] args) {
+        KucingBangsawan kucingBangsawan= new KucingBangsawan("Aya", 100, 30, "Empress");
+        KucingPrajurit kucingPrajurit= new KucingPrajurit("Rian", 120, 30);
+        KucingRakyat kucingRakyat = new KucingRakyat("Salsa", 80, 20);
 
+        System.out.println(kucingBangsawan.status() + "\n");
+        System.out.println(kucingPrajurit.status() + "\n");
+        System.out.println(kucingRakyat.status() + "\n");
+        kucingBangsawan.attack(kucingRakyat);
+        kucingRakyat.attack(kucingBangsawan);
+        kucingPrajurit.attack(kucingBangsawan);
+        System.out.println(kucingBangsawan.status() + "\n");
+        System.out.println(kucingPrajurit.status() + "\n");
+        System.out.println(kucingRakyat.status() + "\n");
+        kucingBangsawan.sleep();
+        kucingPrajurit.sleep();
+        kucingRakyat.sleep();
+        System.out.println(kucingBangsawan.status() + "\n");
+        System.out.println(kucingPrajurit.status() + "\n");
+        System.out.println(kucingRakyat.status() + "\n");
+        kucingRakyat.payTax();
+        kucingPrajurit.payTax();
+        System.out.println(kucingPrajurit.status() + "\n");
+        System.out.println(kucingRakyat.status() + "\n");
+        kucingBangsawan.execute(kucingPrajurit);
+        kucingBangsawan.execute(kucingRakyat);
+        kucingRakyat.sleep();
+        kucingPrajurit.sleep();
+        System.out.println(kucingPrajurit.status() + "\n");
+        System.out.println(kucingRakyat.status() + "\n");
     }
 }
